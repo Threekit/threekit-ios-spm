a832ef4ea33695f80c56efdca0334032318d6994
kwentland <kwentland@threekit.com>
2022-11-24 04:56:18 -0600

    add target

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "Threekit",
-            targets: ["Core"]
+            targets: ["Core", "Threekit_Framework"]
         )
     ],
     dependencies: [
